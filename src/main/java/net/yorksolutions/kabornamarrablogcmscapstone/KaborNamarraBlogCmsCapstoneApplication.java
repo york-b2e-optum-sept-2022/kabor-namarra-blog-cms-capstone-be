47b2bedc9f97003456f9178855e6141d55988bc2
KaborNA <Kabor.namarra@gmail.com>
2022-12-05 10:20:03 -0600

    init

---
+++ src/main/java/net/yorksolutions/kabornamarrablogcmscapstone/KaborNamarraBlogCmsCapstoneApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.kabornamarrablogcmscapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaborNamarraBlogCmsCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaborNamarraBlogCmsCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/kabornamarrablogcmscapstone/KaborNamarraBlogCmsCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.kabornamarrablogcmscapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaborNamarraBlogCmsCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
